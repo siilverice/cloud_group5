7e928df40ba23c09a36476a5f3ba5344a4c32198
silverice <rlt.silverice@gmail.com>
2016-02-07 03:12:48 +0700

    add vm, requirements, storage model

--- cloud_group5/tiramisu/models.py
+++ cloud_group5/tiramisu/models.py
@@ -3,3 +3,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class vm(models.Model):
+	id = models.AutoField(primary_key=True)
+	owner = models.IntegerField()
+	name = models.CharField(max_length=100,unique=True)
+	ip = models.CharField(max_length=16,unique=True)
+	status = models.IntegerField()
+	size = models.FloatField(default=8589.93)
+	cost = models.FloatField(default=429.49)
+
+class requirements(models.Model):
+	vm_name = models.CharField(max_length=100,primary_key=True)
+	latency 	= models.IntegerField()
+	latency_max = models.IntegerField()
+	percentl 	= models.IntegerField()
+	iops_min 	= models.IntegerField()
+	iops  		= models.IntegerField()
+	percenti  	= models.IntegerField()
+	cost  		= models.IntegerField()
+	cost_max  	= models.IntegerField()
+	percentc  	= models.IntegerField()
+	app_type  	= models.IntegerField()
+
+class storage(models.Model):
+	vm_name = models.CharField(max_length=100,primary_key=True)
+	current_pool = models.CharField(max_length=10)
+	appropiate_pool = models.CharField(max_length=10)
--- cloud_group5/tiramisu/views.py
+++ cloud_group5/tiramisu/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'starter.html')
+
+def manage(request):
+	return render(request,'requirements.html')
